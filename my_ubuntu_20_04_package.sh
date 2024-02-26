06ec85379a2d066c057b2c52aa69d0609b2a8b3c
lizijie <425880365@qq.com>
2024-02-26 12:23:43 +0800

    Update my_ubuntu_20_04_package.sh

--- my_ubuntu_20_04_package.sh
+++ my_ubuntu_20_04_package.sh
@@ -2,8 +2,9 @@
 
  # 1. 修改镜像源 
  # @ref: https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
- sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
- # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+sudo mv /etc/apt/sources.list /etc/apt/sources.list_bak
+cat > /etc/apt/sources.list  << EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
 # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
 deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
@@ -20,3 +21,21 @@ deb http://security.ubuntu.com/ubuntu/ focal-security main restricted universe m
 # 预发布软件源，不建议启用
 # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
 # # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+EOF
+
+# intstall Docker
+# @ref https://docs.docker.com/engine/install/ubuntu/
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
