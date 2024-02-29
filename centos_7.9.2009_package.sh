d5da1c421eeeaee993a364f34bcef9d47d9096e2
lizijie <425880365@qq.com>
2024-02-29 16:09:01 +0800

    Update centos_7.9.2009_package.sh

--- centos_7.9.2009_package.sh
+++ centos_7.9.2009_package.sh
@@ -1,5 +1,5 @@
 yum update -y && yum install passwd sudo -y
-sudo yum -y install git make vim tar zip file net-tools nc tcpdump procps-ng lsof perf iperf iotop htop dstat strace sysstat collectl
+sudo yum -y install wget git make vim tar zip file net-tools nc tcpdump procps-ng lsof perf iperf iotop htop dstat strace sysstat collectl
 
 # docker
 # @ref https://docs.docker.com/engine/install/centos/
@@ -11,5 +11,5 @@ sudo docker run hello-world
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 sudo docker run --name mongodb -d -p 27017:27017 -v $(pwd)/data:/data/db -e MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=pass mongo:4.4.28
-
-
+wget https://downloads.mongodb.com/compass/mongodb-mongosh-2.1.5.x86_64.rpm
+rpm -i mongodb-mongosh-2.1.5.x86_64.rpm
