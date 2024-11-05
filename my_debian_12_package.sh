07bf0e58ae34c63810f339c8195cf6e9bf3a0953
lizijie <425880365@qq.com>
2024-11-05 17:16:09 +0800

    Update my_debian_12_package.sh

--- my_debian_12_package.sh
+++ my_debian_12_package.sh
@@ -80,7 +80,15 @@ sudo apt update
 sudo apt install -y --install-recommends winehq-stable
 
 # my dev tools
-sudo apt-get install -y vim wget openssh-client make cmake git ibus-table-wubi net-tools libpcre3-dev libssl-dev perl build-essential curl systemd-timesyncd python3 python3-pip
+sudo apt-get install -y vim wget openssh-client make cmake git ibus-table-wubi net-tools libpcre3-dev libssl-dev perl build-essential curl systemd-timesyncd python3 python3-pip ython3.11-venv
+# 创建python环境
+cd ~/
+python3 -m venv py3env
+source py3env/bin/activate
+# 删除python环境
+deactivate
+rm -f ~/py3env
+
 # 同步网络时间
 sudo timedatectl set-ntp true
 ＃ 设置git环境
