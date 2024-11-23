d0c056b87b2e613b10c553b6f13c2cfc6469e20d
lizijie <425880365@qq.com>
2024-11-23 23:16:44 +0800

    Update my_debian_12_package.sh

--- my_debian_12_package.sh
+++ my_debian_12_package.sh
@@ -128,21 +128,13 @@ pulseaudio --start
 sudo systemctl restart bluetooth
 sudo apt install rfkill
 /usr/sbin/rfkill unblock bluetooth
-:'
-Start the bluetoothctl interactive command. Enter "help" to get a list of available commands.
+# bluetoothctl tip:
+-scan on
+-remove XX:XX:XX:XX:XX:XX, if it had already been paired
+-trust XX:XX:XX:XX:XX:XX
+-pair XX:XX:XX:XX:XX:XX
+-connect XX:XX:XX:XX:XX:XX
 
-    Turn the power to the controller on by entering "power on". It is off by default.
-    Enter "devices" to get the MAC Address of the device with which to pair.
-    Enter device discovery mode with "scan on" command if device is not yet on the list.
-    Turn the agent on with "agent on".
-
-    Enter "pair MAC Address" to do the pairing (tab completion works).
-
-    If using a device without a PIN, one may need to manually trust the device before it can reconnect successfully. Enter "trust MAC Address" to do so.
-
-    Finally, use "connect MAC address" to establish a connection. 
-'
-=======
 # os: debian 12
 
 # isntall nvida drivers
